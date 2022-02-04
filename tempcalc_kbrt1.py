12eefabae7e465ac50e797441d84e63463ce6894
subhradip-paul <subhradip.paul.sp@gmail.com>
2022-02-04 10:53:23 +0100

    temp measurements with KBr added

---
+++ tempcalc_kbrt1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 23:34:00 2021
+
+@author: subhr
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx], idx
+
+
+temp = np.linspace(20,400,4000)
+y0=0.0145*np.ones(np.size(temp));
+y1=5330*(temp ** -2);
+y2=1.42e7*(temp ** -4);
+y3=2.48e9*(temp ** -6);
+y=y0+y1+y2+y3;
+
+t1 = float(input("Enter a number:"));
+
+plt.figure()
+plt.plot(temp,y)
+plt.plot((temp[1], temp[np.size(temp)-1]), (t1, t1), 'k.-')
+
+
+k, l=find_nearest(y, t1)
+
+print(temp[l])
