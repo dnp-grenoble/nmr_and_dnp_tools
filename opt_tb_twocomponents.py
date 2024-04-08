e220901ed22b2ab83a0a9f97118e11086e0bb193
Subhradip Paul <subhradip.paul.sp@gmail.com>
2024-04-08 10:45:17 +0200

    updated optimum tb script

--- opt_tb_twocomponents.py
+++ opt_tb_twocomponents.py
@@ -4,52 +4,50 @@ Created on Tue Jan 25 18:52:52 2022
 
 @author: Paul
 """
-from sympy import symbols, diff, exp, sqrt, lambdify
-import matplotlib.pyplot as plt
+
 import numpy as np
-
-# init_session()
-
-t, b, a, Ta, Tb = symbols('t b a Ta Tb')
-b = 1 - a
-
-s = a * (1 - exp(-t / Ta)) / sqrt(t) + b * (1 - exp(-t / Tb)) / sqrt(t)
-bup = s * sqrt(t)
-k = diff(s, t)
-
-# pprint(s)
+import matplotlib.pyplot as plt
 
 
-# pprint(k)
-t1 = input("Enter the first buildup time: ")
-t2 = input("Enter the second buildup time: ")
+def relax_model(a, ta, tb, beta, xmin, xmax):
+  optd1=np.empty([])
+  index=np.empty([])
+  x=np.linspace(xmin, xmax, 1000000)
+  y=1-np.exp(-x/tb)+a*np.exp(-x/tb)-a*np.exp(-(x/ta)**beta)
+  sens=y/np.sqrt(x)
+  dsens = np.gradient(sens,(xmax-xmin)/1000000)
+  index = np.where(np.diff(np.sign(np.squeeze(dsens))) != 0)[0] + 1
+  if index.size > 0:
+    optd1 = x[index]
+  return x, y, sens, optd1
+
+
+
+t1 = float(input("Enter the first buildup time: "))
+t2 = float(input("Enter the second buildup time: "))
 comp1 = float(input("Enter the topspin value of the first component: "))
 comp2 = float(input("Enter the topspin value of the second component: "))
 
 comp = comp1 / (comp1 + comp2)
 
-y = k.subs({Ta: t1, Tb: t2, a: comp})
-y2 = s.subs({Ta: t1, Tb: t2, a: comp})
-y3 = bup.subs({Ta: t1, Tb: t2, a: comp})
-
-lam_x = lambdify(t, y, modules=['numpy'])
-lam_x2 = lambdify(t, y2, modules=['numpy'])
-lam_x3 = lambdify(t, y3, modules=['numpy'])
-
-x_vals = np.linspace(1, 256, 100000)
-y_vals = lam_x(x_vals)
-y2_vals = lam_x2(x_vals)
-y3_vals = lam_x3(x_vals)
-min_x_val = x_vals[np.argmin(abs(y_vals))]
+x, y_vals, y_sens, opt_tb = relax_model(comp, t1, t2, 1.0, 0.001, 512.)
 
 
-plt.plot(x_vals, abs(y_vals), label=r"$\frac{d \left( \frac{I}{\sqrt{t}} \right) }{dt}$")
-plt.plot(x_vals, abs(y2_vals), label=r"$\frac{I}{\sqrt{t}}$")
-plt.plot(x_vals, y3_vals, label="Intensity(I)")
-plt.plot((min_x_val, min_x_val), (0, np.max(y3_vals)), 'k-.')
-plt.xlabel('Time(s)')
-plt.ylabel('Intensity (arb. u.)')
-plt.legend()
+
+
+fig, ax = plt.subplots()
+ax.plot(x, y_vals, label='Intensity')
+ax.plot(x, y_sens, label='Sensitivity')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Intensity')
+ax.legend(loc = 'best')
+ax.set_ylim([0.0, 1.2])
+    
+if not np.shape(opt_tb):
+    print("No suitable solution found, derivative has **NO** zero crossing.")
+else:
+    ax.axvline(x=opt_tb[0], ls = '--', lw = 0.5, c='magenta')
+    ax.text(x=opt_tb[0]+5, y=1.1, s=np.round(opt_tb[0],2) , fontsize=12)
+    print('The optimum recycle delay is ' + str(np.round(opt_tb[0],2)) + ' s')
+
 plt.show()
-
-print('The optimum d1 should be: ' + str(x_vals[np.argmin(abs(y_vals))]))
